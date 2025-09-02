9f2c62a1247c9565a271b6af56187db98e4384fb
Manoti <hillarymanoti0@gmail.com>
2025-09-03 02:31:41 +0300

    day 2

--- app.py
+++ app.py
@@ -81,3 +81,12 @@
 # else:
 #     print ("Name Looks good!")
 
+weight = int(input("Weight: "))
+unit = input("(L)bs or (K)g: ")
+if unit.upper() == "L":
+    converted = weight * 0.45
+    print(f"You are {converted} kilos")
+else:
+    converted = weight / 0.45
+    print(f"You are {converted} pounds")
+    

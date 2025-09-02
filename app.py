ee24dd3f8e75c86001e37f595eb1917214eeb7aa
Manoti <hillarymanoti0@gmail.com>
2025-09-03 02:46:44 +0300

    day 2

--- app.py
+++ app.py
@@ -81,12 +81,29 @@
 # else:
 #     print ("Name Looks good!")
 
-weight = int(input("Weight: "))
-unit = input("(L)bs or (K)g: ")
-if unit.upper() == "L":
-    converted = weight * 0.45
-    print(f"You are {converted} kilos")
-else:
-    converted = weight / 0.45
-    print(f"You are {converted} pounds")
-    
+# weight = int(input("Weight: "))
+# unit = input("(L)bs or (K)g: ")
+# if unit.upper() == "L":
+#     converted = weight * 0.45
+#     print(f"You are {converted} kilos")
+# else:
+#     converted = weight / 0.45
+#     print(f"You are {converted} pounds")
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i = i + 1
+# print("Done")
+
+# secret_number = 9
+# guess_count = 0
+# guess_limit = 3
+# while guess_count < guess_limit:
+#     guess = int(input("Guess: "))
+#     guess_count += 1
+#     if guess == secret_number:
+#         print("You won!")
+#         break
+# else:
+#     print("Sorry, you failed!")

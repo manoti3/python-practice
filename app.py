4fdc65e468df3a87cb01c8190a13987c89e4c186
Manoti <hillarymanoti0@gmail.com>
2025-09-02 03:37:56 +0300

    submited

---
+++ app.py
@@ -0,0 +1,57 @@
+# name = "manoti"
+# print ("Hello,  " + name)
+
+# course = "Python for Beginners"
+# print(course[1])
+
+# name = "janeffer"
+# print(name[1: -1])
+
+# first = 'John'
+# last = 'smith'
+# message = first + ' [' + last + '] is a coder'
+# msg = f'{first} [{last}] is a coder'
+# print(message)
+# print(msg)
+
+# course = 'Python for Beginners'
+# print(len(course))
+# print(course.upper())
+# print(course.lower())
+# print(course.find('P'))
+# print(course.replace('Beginners', 'Absolute Beginners'))
+# print('Python' in course)
+# print(course.title())
+
+# print(10 - 3)
+
+# x = 10 + 3 * 2 ** 2
+# print(x)
+
+# x = (2 + 3) * 10 - 3
+# print(x)
+
+# x = 2.9
+# print(round(x))
+# print(abs(-2.9))
+
+# is_hot = False
+# is_cold = False
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("It's a cold day")
+#     print("Wear warm clothes")
+# else:
+#     print("It's a lovely day")
+# print("Enjoy your day")
+
+# price = 1000000
+# has_good_credit = True
+
+# if has_good_credit:
+#     down_payment = 0.1 * price
+# else:
+#     down_payment = 0.2 * price
+# print(f'Down payment: ${down_payment}')
